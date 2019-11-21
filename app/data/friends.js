0747e10969549c987a42699eb15b06f49604b139
Carmel Meyer <cemeyer@elkera.com>
2019-11-21 17:15:55 -0500

    Add logic to friends.js

--- app/data/friends.js
+++ app/data/friends.js
@@ -162,4 +162,42 @@ var friendArray = [
     }
 ]
 
+
+
+
+var newFriend = 
+{
+        "name": "Maeby Funke",
+        "photo": "",
+        "scores": [
+            "2", 
+            "3", 
+            "2", 
+            "4",
+            "3",
+            "3",
+            "2",
+            "1",
+            "3",
+            "5"
+        ]
+    }
+
+
+    var friendDiff = []
+    // for loop that looks at each friend in the friend array
+    for (var i = 0; i < friendArray.length; i++) {
+        var totalDiff = 0;
+        // inner for loop will look at the scores of each friend
+        // as we loop through the friend array
+        for (var j = 0; j < friendArray[i].scores.length; j++) {
+            var diff = Math.abs(parseInt(newFriend.scores[j]) - parseInt(friendArray[i].scores[j]));
+            totalDiff += diff;
+        }
+        friendDiff.push(totalDiff);
+    }
+    // console.log("3" - "1")
+    // find index of friendDiff with the small value
+    // that will be the person that answered the question the closest
+
 module.exports = friendArray;
